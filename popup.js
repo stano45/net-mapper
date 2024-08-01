c961cc29e3151966d3b9656fe176a5ab5cc0f70f
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-08-01 12:03:12 +0200

    Cache geolocation data

--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   logToBackground('Popup DOMContentLoaded');
 
-  let downloadBtn = document.getElementById('download-btn');
   let loadingDiv = document.getElementById('loading');
 
-  // Load IP addresses and display them
   chrome.storage.local.get({ips: []}, function(result) {
     const ips = result.ips || [];
     loadingDiv.style.display = 'block';
@@ -31,7 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
       const uniqueIps = Object.keys(ipCounts);
       const geoData = await fetchGeolocationData(uniqueIps);
 
-      // Add counts to geoData
       geoData.forEach(data => {
         data.count = ipCounts[data.query];
       });
@@ -53,7 +50,6 @@ document.addEventListener('DOMContentLoaded', function() {
 async function fetchGeolocationData(ips) {
   logToBackground('Fetching geolocation data for IPs: ' + JSON.stringify(ips));
 
-  // Retrieve cached geolocation data from chrome.storage.local
   let cachedData = {};
   try {
     cachedData = await new Promise((resolve, reject) => {
@@ -95,7 +91,6 @@ async function fetchGeolocationData(ips) {
         let batchData = await response.json();
         geoData.push(...batchData);
 
-        // Update cached data with new batch data
         batchData.forEach(item => {
           if (item.status === 'success') {
             cachedData[item.query] = item;
@@ -107,8 +102,6 @@ async function fetchGeolocationData(ips) {
       }
     }
 
-
-    // Store updated cache in chrome.storage.local
     try {
       await new Promise((resolve, reject) => {
         chrome.storage.local.set({ geoDataCache: cachedData }, () => {
@@ -127,7 +120,6 @@ async function fetchGeolocationData(ips) {
     logToBackground('No new IPs to fetch');
   }
 
-  // Combine cached data with newly fetched data
   ips.forEach(ip => {
     if (cachedData[ip]) {
       geoData.push(cachedData[ip]);
@@ -157,14 +149,13 @@ function loadMapWithGeolocationData(ips) {
       data.count = ipCounts[data.query];
     });
 
-    let map = L.map('map').setView([20, 0], 2); // Adjust the initial view
+    let map = L.map('map').setView([20, 0], 2);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
-    // Add markers with click events for each location
     geoData.forEach(data => {
       if (data.lat && data.lon) {
         let marker = L.circle([data.lat, data.lon], {
@@ -177,18 +168,16 @@ function loadMapWithGeolocationData(ips) {
         marker.bindPopup(`<b>IP:</b> ${data.query}<br><b>Times Accessed:</b> ${data.count}<br><b>Location:</b> ${data.city}, ${data.country}<br><b>ISP:</b> ${data.isp}<br><b>Org:</b> ${data.org}<br><b>AS:</b> ${data.as}`);
       }
     });
+    
 
-    // Add custom button to map
-    let customButton = L.Control.extend({
+    let refreshButton = L.Control.extend({
       options: {
-        position: 'topleft' // 'topleft', 'topright', 'bottomleft', 'bottomright'
+        position: 'topleft'
       },
 
       onAdd: function (map) {
-        // Create a container for the button
         let container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
 
-        // Set styles for the button
         container.style.backgroundColor = 'white'; 
         container.style.width = '30px';
         container.style.height = '30px';
@@ -197,10 +186,8 @@ function loadMapWithGeolocationData(ips) {
         container.style.cursor = 'pointer';
         container.style.fontSize = '18px';
 
-        // Set the button content
-        container.innerHTML = '&#8634;'; // Example icon (Power symbol)
+        container.innerHTML = '&#8634;';
 
-        // Add a click event listener
         container.onclick = function(){
           chrome.storage.local.get({ips: []}, function(result) {
             loadMapWithGeolocationData(result.ips || []);
@@ -211,13 +198,74 @@ function loadMapWithGeolocationData(ips) {
       }
     });
 
-    // Add the custom button to the map
-    map.addControl(new customButton());
+    let downloadButton = L.Control.extend({
+      options: {
+        position: 'topleft'
+      },
 
+      onAdd: function (map) {
+        let container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
 
+        container.style.backgroundColor = 'white'; 
+        container.style.width = '30px';
+        container.style.height = '30px';
+        container.style.lineHeight = '30px';
+        container.style.textAlign = 'center';
+        container.style.cursor = 'pointer';
+        container.style.fontSize = '18px';
+
+        container.innerHTML = '&#8595;';
+
+        container.onclick = function(){
+          chrome.storage.local.get({ips: []}, function(result) {
+            const ips = result.ips || [];
+            downloadIpGeolocationData(ips);
+          });
+        }
+
+        return container;
+      }
+    });
+
+    map.addControl(new refreshButton());
+    map.addControl(new downloadButton());
     map.invalidateSize();
   });
 }
+
+function downloadIpGeolocationData(ips) {
+  if (ips.length === 0) {
+    alert("No IPs to download.");
+    return;
+  }
+  
+  const ipCounts = ips.reduce((acc, ip) => {
+    acc[ip] = (acc[ip] || 0) + 1;
+    return acc;
+  }, {});
+
+  const uniqueIps = Object.keys(ipCounts);
+
+  fetchGeolocationData(uniqueIps).then(geoData => {
+    geoData.forEach(data => {
+      data.count = ipCounts[data.query];
+    });
+
+    const blob = new Blob([JSON.stringify(geoData, null, 2)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    chrome.downloads.download({
+      url: url,
+      filename: 'net_mapper_ip_geolocation_data.json',
+      saveAs: true
+    });
+
+    loadingDiv.style.display = 'none';
+  }).catch(error => {
+    console.error('Error fetching geolocation data:', error);
+  });
+}
+
 
 function logToBackground(message) {
   chrome.runtime.sendMessage({log: message}, function(response) {
